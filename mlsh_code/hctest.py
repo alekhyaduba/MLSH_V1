769090bc54d463caaf67996960fc7b16ed270992
nitin <alekhya.ramarao@gmail.com>
2022-05-05 20:43:57 -0500

    first commit

---
+++ mlsh_code/hctest.py
@@ -0,0 +1,11 @@
+import gym
+# from mlsh_code.mujoco_env import MujocoEnv
+
+from gym.envs.mujoco.mujoco_env import MujocoEnv
+
+
+# MujocoEnv.__init__()
+# env = gym.make("HalfCheetahForwardBackward-v1")
+env = gym.make("HalfCheetah-v2")
+
+print("done")
---
+++ test_envs/test_envs/__init__.py
@@ -0,0 +1,37 @@
+import logging
+from gym.envs.registration import register
+
+logger = logging.getLogger(__name__)
+
+register(
+    id='MovementBandits-v0',
+    entry_point='test_envs.envs:MovementBandits',
+    # timestep_limit=50,
+)
+
+register(
+    id='KeyDoor-v0',
+    entry_point='test_envs.envs:KeyDoor',
+    # timestep_limit=100,
+)
+
+register(
+    id='Allwalk-v0',
+    entry_point='test_envs.envs:Allwalk',
+    # timestep_limit=50,
+)
+
+register(
+    id='Fourrooms-v0',
+    entry_point='test_envs.envs:Fourrooms',
+    # timestep_limit=100,
+    # reward_threshold = 1,
+)
+
+
+register(
+    id='HalfCheetahForwardBackward-v2',
+    entry_point='test_envs.envs:HalfCheetahForwardBackwardEnv',
+    max_episode_steps=100,
+    # timestep_limit=1000,
+)
